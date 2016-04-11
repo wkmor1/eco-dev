f398a7d6acb2ec614e21d8402073f599d8f4d303
wkmor1 <wkmor1@gmail.com>
2016-04-11 15:11:52 +1000

    Update userconf.sh

--- userconf.sh
+++ userconf.sh
@@ -4,20 +4,17 @@ USERID=${USERID:=1000}
 ROOT=${ROOT:=FALSE}
 export HOME=/home/$USER
 
-# Add user
 useradd -u $USERID $USER
 addgroup $USER staff
 echo "$USER:$PASSWORD" | chpasswd
 mkdir -p $HOME/.jupyter/
 mv jupyter_notebook_config.py $_
 
-## Configure git for the User. Since root is running this script, cannot use `git config`
 echo -e "[user]\n\tname = $USER\n\temail = $EMAIL\n\n[credential]\n\thelper = cache\n" > $HOME/.gitconfig
 
-# Use Env flag to know if user should be added to sudoers
 if [ "$ROOT" == "TRUE" ]
-        then
-                adduser $USER sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+  then
+    adduser $USER sudo && echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 fi
 
 env | cat >> /etc/R/Renviron
