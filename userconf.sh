90982f5232fa7be0e811be9f0cf3596bdb540a94
wkmor1 <1680870+wkmor1@users.noreply.github.com>
2018-05-09 11:28:01 +1000

    Update userconf.sh

--- userconf.sh
+++ userconf.sh
@@ -8,14 +8,12 @@ useradd -u $USERID $USER
 addgroup $USER staff
 echo "$USER:$PASSWORD" | chpasswd
 chmod go-rx /usr/bin/passwd
-mkdir -p $HOME/.jupyter/
-mv jupyter_notebook_config.py $_
 
 echo -e "[user]\n\tname = $USER\n\temail = '<>'\n\n[credential]\n\thelper = cache --timeout=31536000\n" > $HOME/.gitconfig
 
 if [ "$ROOT" == "TRUE" ]
   then
-    adduser $USER sudo && echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers && echo "$USER ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/notebook
+    adduser $USER sudo && echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 fi
 
 env | cat >> /etc/R/Renviron
