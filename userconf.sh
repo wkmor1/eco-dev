0e2da4f4fa3a666a0ba7cde0add340251202d3cc
wkmor1 <1680870+wkmor1@users.noreply.github.com>
2019-06-05 13:30:55 +0300

    Update userconf.sh

--- userconf.sh
+++ userconf.sh
@@ -8,6 +8,7 @@ useradd -u $USERID $USER
 addgroup $USER staff
 echo "$USER:$PASSWORD" | chpasswd
 chmod go-rx /usr/bin/passwd
+mkdir -p $HOME
 
 echo -e "[user]\n\tname = $USER\n\temail = '<>'\n\n[credential]\n\thelper = cache --timeout=31536000\n" > $HOME/.gitconfig
 
@@ -16,6 +17,6 @@ if [ "$ROOT" == "TRUE" ]
     adduser $USER sudo && echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 fi
 
-env | cat >> /etc/R/Renviron
+env | cat >> /usr/local/lib/R/etc/Renviron
 
 chown -R $USER:$USER $HOME
