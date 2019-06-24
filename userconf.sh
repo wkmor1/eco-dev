db0718101632b68dcd4172bbf270c21513d959a5
wkmor1 <1680870+wkmor1@users.noreply.github.com>
2019-06-24 09:37:50 +0300

    Update userconf.sh

--- userconf.sh
+++ userconf.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+USER=${USER:=rstudio}
+PASSWORD=${PASSWORD:=rstudio}
+EMAIL=${EMAIL:="<>"}
 USERID=${USERID:=1000}
 ROOT=${ROOT:=FALSE}
 export HOME=/home/$USER
@@ -10,13 +13,13 @@ echo "$USER:$PASSWORD" | chpasswd
 chmod go-rx /usr/bin/passwd
 mkdir -p $HOME
 
-echo -e "[user]\n\tname = $USER\n\temail = '<>'\n\n[credential]\n\thelper = cache --timeout=31536000\n" > $HOME/.gitconfig
+echo -e "[user]\n\tname = $USER\n\temail = $EMAIL\n\n[credential]\n\thelper = cache --timeout=31536000\n" > $HOME/.gitconfig
 
 if [ "$ROOT" == "TRUE" ]
   then
     adduser $USER sudo && echo "%sudo ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 fi
 
-env | cat >> /usr/local/lib/R/etc/Renviron
+echo "PATH=${PATH}" >> /usr/local/lib/R/etc/Renviron
 
 chown -R $USER:$USER $HOME
