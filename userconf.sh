90baf39234fb35e9688e83dd4d0a1175f54bed07
wkmor1 <1680870+wkmor1@users.noreply.github.com>
2019-08-03 12:04:16 +0300

    No longer overwrite user git configuration

--- userconf.sh
+++ userconf.sh
@@ -2,7 +2,6 @@
 
 USER=${USER:=rstudio}
 PASSWORD=${PASSWORD:=rstudio}
-EMAIL=${EMAIL:="<>"}
 USERID=${USERID:=1000}
 ROOT=${ROOT:=FALSE}
 export HOME=/home/$USER
@@ -12,8 +11,6 @@ addgroup $USER staff
 echo "$USER:$PASSWORD" | chpasswd
 chmod go-rx /usr/bin/passwd
 mkdir -p $HOME
-
-echo -e "[user]\n\tname = $USER\n\temail = $EMAIL\n\n[credential]\n\thelper = cache --timeout=31536000\n" > $HOME/.gitconfig
 
 if [ "$ROOT" == "TRUE" ]
   then
